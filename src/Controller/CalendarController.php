f3b47096853f27d45f0557793ae22ea9dfad02da
Wojciech <wojciech.cwiklinski.2001@gmail.com>
2024-12-05 13:38:52 +0100

    test

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -4,19 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'app_calendar')]
-    public function index(): Response
+    public function index(
+//        #[MapQueryParameter(name: 'query')]
+//        array $queries,
+    ): Response
     {
         $cssContent = file_get_contents('../assets/styles/app.css');
         $jsContent = file_get_contents('../assets/app.js');
 
+//        print_r($queries);
+
+
+
         return $this->render('calendar/index.html.twig', [
             'cssContent' => $cssContent,
             'jsContent' => $jsContent,
+//            'queries' => $queries,
         ]);
     }
 }
